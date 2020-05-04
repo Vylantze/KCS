3bd43bb10a596d7732a377aa4956886f6ac4ab5f
darrylvr <darryl@vrcollab.com>
2020-05-05 00:32:06 +0800

    Added the room background and got it to display properly.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="KCE Logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <Room />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Room from "./components/Room.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Room
   }
-}
+};
 </script>
 
-<style>
-#app {
+<style lang="less" scoped>
+.app {
+  height: 100%;
+  width: 100%;
+  display: flex;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  //text-align: center;
+  //color: #2c3e50;
+  //margin-top: 60px;
 }
 </style>
---
+++ src/components/Room.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="room-container center-div">
+    <div class="room fixed">
+      <img src="backgrounds/room_background.png" class="background" />
+      <img src="backgrounds/room_window.png" class="background ontop" />
+      <img src="backgrounds/room_objects.png" class="background ontop" />
+    </div>
+  </div>
+</template>
+
+
+<style lang="less" scoped>
+.room-container {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+
+  .room {
+    height: 100%;
+
+    .background {
+      height: 100%;
+      min-width: 800;
+      object-fit: cover;
+      user-select: none;
+
+      &.ontop {
+        position: absolute;
+        left: 0;
+        top: 0;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "Room"
+};
+</script>
