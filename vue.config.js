2877e565f7e1f62c11a9bd65ff46561e64895f11
Darryl <darryl@vrcollab.com>
2021-12-07 17:53:47 +0800

    Try to deploy to github pages.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js
 module.exports = {
   // options...
+  publicPath: process.env.NODE_ENV === 'production' ? 'KCS' : '/',
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
