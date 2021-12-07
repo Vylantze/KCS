e1eb47e9c28f6f9fa1730b7bffe9776530159910
Darryl <darryl@vrcollab.com>
2021-12-07 19:22:36 +0800

    Readd the dist. Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // vue.config.js
 module.exports = {
   // options...
-  publicPath: process.env.NODE_ENV === 'production' ? 'KCS' : '/',
+  publicPath: '/',
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
